9648388d034f3d97c68d3db5f5c87adb297ff4ed
Erlich Bachman <dretzam@gmail.com>
2018-07-19 13:05:07 +0100

    still fixing footer 6

--- src/components/village/neighborhoods/blocks/houses/Menu List.js
+++ src/components/village/neighborhoods/blocks/houses/Menu List.js
@@ -6,6 +6,39 @@ export default class MenuList extends React.Component {
        render() {
          return (
            <section className="menu_list">
+          <Row>
+             <Col lg={6} md={6} sm={6} xs={6}>
+              <Row>
+
+          <p><Link to="/eshop_list">e-shops
+           </Link></p>
+
+           <p><Link to="/why_us">Why Us
+           </Link></p>
+
+           <p><Link to="/howto">How To
+           </Link></p>
+
+           <p><Link to="/faq">FAQ
+           </Link></p>
+
+            <p><Link to="/feedback">Feedback
+           </Link></p>
+
+           <p><Link to="/about">About Us
+           </Link></p>
+
+            <p><Link to="/contact_us">Contact Us
+           </Link></p>
+
+               </Row>
+              </Col>
+
+
+
+
+             <Col lg={6} md={6} sm={6} xs={6}>
+              <Row>
            {this.props.logged_in ? (
 
              <Col lg={6} md={6} sm={12} xs={12}>
@@ -21,10 +54,20 @@ export default class MenuList extends React.Component {
            ) : (
 
             <Col lg={6} md={6} sm={12} xs={12}>
+             <p><Link to = "/signin">
+               Signin
+             </Link></p>
+             <p><Link to = "/signup">
+               Signup
+             </Link></p>
 
-            </Col>
+             </Col>
 
            )}
+             </Row>
+            </Col>
+           </Row>
+
 
            </section>
          )
