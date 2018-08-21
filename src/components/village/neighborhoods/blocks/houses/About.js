f2edf72ec6012979d046da7fdd39594ab13065ce
Erlich Bachman <dretzam@gmail.com>
2018-08-21 23:39:43 +0100

    more updates

--- src/components/village/neighborhoods/blocks/houses/About.js
+++ src/components/village/neighborhoods/blocks/houses/About.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Image} from 'react-bootstrap'
 
 export default class About extends React.Component {
        render() {
          return (
            <section className="about" id="about">
             <Row>
-             <Col lg={6} lgOffset={1} md={6} mdOffset={1} sm={6} smOffset={1} xs={6} xsOffset={1}>
+            <Col lg={6} lgOffset={1} md={6} mdOffset={1} sm={6} smOffset={1} xs={6} xsOffset={1}>
               <Row>
 
           <p><Link to="/eshop_list">e-shops
@@ -37,7 +37,7 @@ export default class About extends React.Component {
 
 
 
-             <Col lg={5} md={5} sm={5} xs={5}>
+            <Col lg={5} md={5} sm={5} xs={5}>
               <Row>
            {this.props.logged_in ? (
 
@@ -46,8 +46,8 @@ export default class About extends React.Component {
                Rent an e-shop
              </Link></p>
              <p><Link to = "/product_valuation">
-              Business Mode
-            </Link></p>
+               Business Mode
+             </Link></p>
              <p><Link to = "/logout">
                Logout
              </Link></p>
@@ -70,6 +70,33 @@ export default class About extends React.Component {
              </Row>
             </Col>
            </Row>
+
+          <Row>
+           <Col lg={8} lgOffset={3} md={8} mdOffset={3} sm={12} xs={12}>
+             <Row>
+              <Col lg={2} md={2} sm={3} smOffset={2} xs={3} xsOffset={2}>
+
+                <a href="https://web.facebook.com/Iwansell-group-270682653560747/?ref=br_rs">
+                  <Image width="50px" height="50px" src={ require ('./images/facebook.png') } alt="facebook" responsive/>
+                </a>
+              </Col>
+
+              <Col lg={2} md={2} sm={3} xs={3}>
+                <a href="https://twitter.com/IwansellG">
+                  <Image width="50px" height="50px" src={ require ('./images/twitter.png') } alt="twitter" responsive/>
+                </a>
+              </Col>
+
+              <Col lg={2} md={2} sm={3} xs={3}>
+                <a href="https://instagram.com/iwansell_group">
+                  <Image width="60px" height="60px" src={ require ('./images/instagram.png') } alt="instagram" responsive/>
+                </a>
+              </Col>
+
+             </Row>
+            </Col>
+          </Row>
+
 
            </section>
          )
