595efe7e4511009a358019daf3829d7dcf70dedf
Retzam Tarle <Younggregs>
2020-01-20 22:15:51 +0100

    push push

--- src/components/village/neighborhoods/E Shop Navigation.js
+++ src/components/village/neighborhoods/E Shop Navigation.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Row,Col} from 'react-bootstrap'
 import EShopSearch from './blocks/houses/E Shop Search'
-import EShopName from './blocks/houses/EShop Name'
 
 export default class EShopNavigation extends React.Component {
 
@@ -9,41 +8,13 @@ export default class EShopNavigation extends React.Component {
          return (
            <section>
                
-
-                 <Col lg={12} md={12} smHidden xsHidden>
                  <div className="eshop-navigation">
                   <Row>
                    
                     <EShopSearch eshop_name = {this.props.eshop_name} eshop_id = {this.props.eshop_id}/>
-                     
                    
                   </Row>
                   </div>
-                  </Col>
-
-
-                  <div className="sm-eshop-navigation">
-                  <Col sm={12} xs={12} lgHidden mdHidden>
-                  <Row>
-                   <Col sm={12} xs={12}>
-                     <div className="smaller">
-                     <EShopName eshop_name = {this.props.eshop_name}/>
-                     </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                  <Col sm={12} xs={12}>
-                     
-                    <EShopSearch eshop_name = {this.props.eshop_name} eshop_id = {this.props.eshop_id}/>
-                     
-                    </Col>
-                  </Row>
-
-                 
-                  </Col>
-                  </div>
-
-
                 
                 <br /><br /><br />
            </section>
