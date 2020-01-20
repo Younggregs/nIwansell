91a1bd5c774232ff118c924f5679da89f9bfcd33
Retzam Tarle <Younggregs>
2020-01-20 22:13:28 +0100

    push push

--- src/components/village/neighborhoods/blocks/EShop Store 2.js
+++ src/components/village/neighborhoods/blocks/EShop Store 2.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import Heading from './houses/Heading'
 import ProductImage from './houses/Product Image'
 
